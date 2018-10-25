0af0194ca98a53a49b5dc07f9184d945440f5d4e
xindi-wang <wangcd94@gmail.com>
2018-10-25 19:52:39 -0400

    constructing filter

--- js/draw.js
+++ js/draw.js
@@ -1,9 +1,48 @@
 $(document).ready(function () {
     loadData();
+    drawCircleAnimation();
 });
 
 function loadData() {
   d3.csv("data/data.csv",function(d){
 
   })
+}
+
+function toggle(){
+  if($("#temp").is(":checked")){
+    $(".tempfilter").css("display", "block");
+  }else{
+    $(".tempfilter").css("display", "none");
+  }
+}
+
+function drawCircleAnimation(){
+  var svg = d3.select("#chart")
+     .append("svg")
+     .attr("width", 500)
+     .attr("height", 300)
+     .append("g")
+
+  var circle =  svg.append("circle")
+      .attr("cx",250)
+      .attr("cy",150)
+      .style("fill","blue")
+      .attr("r",20)
+
+  var pm = svg.append("text")
+      .attr("dx", 250)
+      .attr("dy", 150)
+	    .text("100")
+
+  repeat();
+
+  function repeat(){
+    circle
+    .transition()
+    .attr('r', function(d){
+      return Math.random()*50
+    })
+    .on("end", repeat);
+  }
 }
